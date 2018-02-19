3709afd0f63c46f1a15ba495ced2f6282d49a5f0
Cameron Fabbri <cameronfabbri@gmail.com>
2018-02-19 13:00:54 -0600

    added example load features file

---
+++ load_features.py
@@ -0,0 +1,27 @@
+'''
+
+   Base file for using the features that were computed and stored in the pickle file.
+   This is tensorflow agnostic
+
+   Ex. python load_features.py inception_v1_features.pkl
+
+'''
+
+import numpy as np
+import sys
+import cPickle as pickle
+
+if __name__ == '__main__':
+
+   try:
+      pkl_file = open(sys.argv[1], 'rb')
+      features = pickle.load(pkl_file)
+   except:
+      print 'Must provide a pickle file'
+      exit()
+
+   for image, feature in features.iteritems():
+      print image, ':', feature.shape
+
+      # do whatever you want with features
+
