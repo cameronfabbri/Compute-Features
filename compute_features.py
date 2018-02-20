808f7009906055f23130935f7afcfd89c76fb941
Cameron Fabbri <cameronfabbri@gmail.com>
2018-02-20 16:52:26 -0600

    update

--- compute_features.py
+++ compute_features.py
@@ -50,8 +50,7 @@ if __name__ == '__main__':
    model           = a.model
    checkpoint_file = a.checkpoint_file
    
-   if model == 'inception_v1': height, width, channels = 224, 224, 3
-   if model == 'inception_v2': height, width, channels = 224, 224, 3
+   if model == 'inception_v1' or model == 'inception_v2' or model == 'inception_v3': height, width, channels = 224, 224, 3
    if model == 'vgg19':        height, width, channels = 224, 224, 3
 
    x = tf.placeholder(tf.float32, shape=(1, height, width, channels))
@@ -68,6 +67,12 @@ if __name__ == '__main__':
       arg_scope = inception_v2_arg_scope()
       with slim.arg_scope(arg_scope):
          logits, end_points = inception_v2(x, is_training=False, num_classes=1001)
+         features = end_points['AvgPool_1a']
+   elif model == 'inception_v3':
+      from inception_v3 import *
+      arg_scope = inception_v3_arg_scope()
+      with slim.arg_scope(arg_scope):
+         logits, end_points = inception_v3(x, is_training=False, num_classes=1001)
          features = end_points['AvgPool_1a']
    elif model == 'vgg_19':
       from vgg import *
